bda357efde4a6d50722e0993e4ac1cff48d7eecd
Sleepy Strawberry <narendarczyk@o2.pl>
2023-11-02 18:47:48 +0100

    changed photos of cats and comments, changed colors from white to dark

--- data.js
+++ data.js
@@ -1,58 +1,57 @@
-export const tweetsData = [   
-    {
-        handle: `@TrollBot66756542 💎`,
-        profilePic: `images/troll.jpg`,
-        likes: 27,
-        retweets: 10,
-        tweetText: `Buy Bitcoin, ETH Make 💰💰💰 low low prices. 
-            Guaranteed return on investment. HMU DMs open!!`,
-        replies: [],
-        isLiked: false,
-        isRetweeted: false,
-        uuid: '4b161eee-c0f5-4545-9c4b-8562944223ee',
-    },    
-    {
-        handle: `@Elon ✅`,
-        profilePic: `images/musk.png`,
-        likes: 6500,
-        retweets: 234,
-        tweetText: `I need volunteers for a one-way mission to Mars 🪐. No experience necessary🚀`,
-        replies: [
-                  {
-                handle: `@TomCruise ✅`,
-                profilePic: `images/tcruise.png`,
-                tweetText: `Yes! Sign me up! 😎🛩`,
-            },
-                  {
-                handle: `@ChuckNorris ✅`,
-                profilePic: `images/chucknorris.jpeg`,
-                tweetText: `I went last year😴`,
-            },
-        ],
-        isLiked: false,
-        isRetweeted: false,
-        uuid: '3c23454ee-c0f5-9g9g-9c4b-77835tgs2',
-    },
-        {
-        handle: `@NoobCoder12`,
-        profilePic: `images/flower.png`,
-        likes: 10,
-        retweets: 3,
-        tweetText: `Are you a coder if you only know HTML?`,
-        replies: [
-            {
-                handle: `@StackOverflower ☣️`,
-                profilePic: `images/overflow.png`,
-                tweetText: `No. Obviosuly not. Go get a job in McDonald's.`,
-            },
-            {
-                handle: `@YummyCoder64`,
-                profilePic: `images/love.png`,
-                tweetText: `You are wonderful just as you are! ❤️`,
-            },
-        ],
-        isLiked: false,
-        isRetweeted: false,
-        uuid: '8hy671sff-c0f5-4545-9c4b-1237gyys45',
-    },     
+export const tweetsData = [
+  {
+    handle: `@Mia24234 💎`,
+    profilePic: `images/orangecat.jpg`,
+    likes: 27,
+    retweets: 10,
+    tweetText: `Just had a cozy nap in my favorite sunspot ☀️😺 Time to plot world domination from the windowsill 🐾😼`,
+    replies: [],
+    isLiked: false,
+    isRetweeted: false,
+    uuid: '4b161eee-c0f5-4545-9c4b-8562944223ee',
+  },
+  {
+    handle: `@Pushyek Official 🐻`,
+    profilePic: `images/pushyek.png`,
+    likes: 6500,
+    retweets: 234,
+    tweetText: `Just caught the red dot, and I am victorious! 🏆 Now I'm off to groom my royal fur. 😼👑🐾`,
+    replies: [
+      {
+        handle: `@Persian cats forever ✅`,
+        profilePic: `images/closecat.png`,
+        tweetText: `Red dot triumphs are the best kind of victories, aren't they? 🏆😺 After my recent feather wand conquest, I like to follow it up with a regal nap on the human's pillow. 😸🛏️`,
+      },
+      {
+        handle: `@Carbon ♣️`,
+        profilePic: `images/graycat.jpg`,
+        tweetText: `Ah, the elusive red dot – my eternal foe! 🐾🔴 I celebrated my latest 'dot capture' by demanding an extra serving of treats from the staff. A true ruler knows how to seize opportunities! 😼👑`,
+      },
+    ],
+    isLiked: false,
+    isRetweeted: false,
+    uuid: '3c23454ee-c0f5-9g9g-9c4b-77835tgs2',
+  },
+  {
+    handle: `@Xx Yuno xX ☠️`,
+    profilePic: `images/flower.png`,
+    likes: 10,
+    retweets: 3,
+    tweetText: `Rainy day, again?! 🌧️ I shall express my displeasure by knocking things off the table one by one. 😼🌂`,
+    replies: [
+      {
+        handle: `@Pumpkin 🎃`,
+        profilePic: `images/pumpkin.jpg`,
+        tweetText: `I feel you, buddy! Rainy days are the absolute worst. My coping strategy involves staring out the window with an extra helping of tuna. 🌧️🐟`,
+      },
+      {
+        handle: `@TrueTiger64`,
+        profilePic: `images/tiger.jpg`,
+        tweetText: `Rainy days call for extra naps in the comfiest spots. Time to curl up on the warm laundry pile and forget about the world outside! 😸🛏️`,
+      },
+    ],
+    isLiked: false,
+    isRetweeted: false,
+    uuid: '8hy671sff-c0f5-4545-9c4b-1237gyys45',
+  },
 ]
